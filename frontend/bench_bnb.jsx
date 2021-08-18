4ff036b0367d278740d74fff89349156c6532772
siascone <spencer.iascone@gmail.com>
2021-08-18 13:19:17 -0600

    start session api util

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+
     const root = document.getElementById('root');
     ReactDom.render(<h1>Welcome to BenchBnB</h1>, root);
 });
